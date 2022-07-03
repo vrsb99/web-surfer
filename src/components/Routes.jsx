9b940335c1e653c2053c56822af7750c5cb30396
Vignesh <vigneshrsb99@gmail.com>
2022-07-03 21:24:20 +0800

    First Commit

---
+++ src/components/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom';
+
+export const Routes = () => {
+  return (
+    <div>
+        <h1>Routes</h1>
+
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App.js';
+import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+ReactDom.render(
+    <Router>
+     <App/>
+    </Router>
+, document.getElementById('root'));
